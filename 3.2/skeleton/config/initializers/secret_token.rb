25ed64bdd5a6ca47f7422646551dd1061e6ecbde
Yutaka Tachibana <yutaka.paradigm.shift@gmail.com>
2015-07-15 12:23:21 +0900

    initial commit

---
+++ 3.2/skeleton/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skeleton::Application.config.secret_token = '3da7b6dc42446ef31e61291af92f3adbeee6decdf5c2beb117b7df0d4dddab240ee54771153c1de3c7ba2faeed3b2dae6b0ffff993f598e4890f0a8c5384ab68'
---
+++ 4.0/skeleton/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skeleton::Application.config.secret_key_base = '0c5f3d0e486599942b00c0055260923e42ce2a658319c8532bcbe481108cb157f52bdcf2b86dbdda388ea1f4c152e233e333c1609f9edf29ada66767bc6ee433'
